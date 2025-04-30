c5923c973d99a9c10275aa8e62c324cc1140b10b
Visar <visarmullaj07@gmail.com>
2025-04-30 18:41:32 +0200

    Added not-found for nonexisting pages

---
+++ src/app/not-found.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const NotFound = () => {
+  return (
+    <div className="flex justify-center items-center w-screen h-screen flex-col gap-20">
+      <h1 className="text-8xl font-bold">404</h1>
+      <div className="flex justify-center items-center flex-col gap-6">
+        <h2 className="text-2xl">
+          The page you&apos;re requesting does not exist
+        </h2>
+        <Link href={"/"}>
+          <Button className="text-xl" size="lg">
+            Go Home
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+export default NotFound;
