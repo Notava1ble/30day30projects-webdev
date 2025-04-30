981741c41e71ec3944733253f21d7ff0de5bb509
Visar <visarmullaj07@gmail.com>
2025-04-30 18:48:18 +0200

    updated page.tsx to include links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Main Page</div>;
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen gap-20">
+      <h1 className="text-6xl font-bold">Try some links</h1>
+      <ul className="flex flex-row items-center justify-center gap-10">
+        <li className="text-center">
+          <p className="mb-2">Look at our about page</p>
+          <Link href={"/about"}>
+            <Button>about</Button>
+          </Link>
+        </li>
+        <li className="text-center">
+          <p className="mb-2">Look at our contacts page</p>
+          <Link href={"/contacts"}>
+            <Button>contacts</Button>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
