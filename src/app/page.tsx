3aba733f81c1e8f1f7aa51cde4acc880531d98b0
Visar <visarmullaj07@gmail.com>
2025-05-01 14:00:16 +0200

    fetched crypto data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
-export default function Home() {
-  return <div>Main Page</div>;
+export default async function Page() {
+  const res = await fetch(
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd",
+    {
+      cache: "no-store",
+    }
+  );
+
+  const cryptoData = await res.json();
+  if (cryptoData?.status?.error_code == 429)
+    return (
+      <main className="w-screen h-screen flex-col flex justify-center items-center">
+        <p>status code: {cryptoData.status.error_code}</p>
+        <p>message: {cryptoData.status.error_message}</p>
+      </main>
+    );
+
+  return (
+    <main className="w-screen h-screen flex justify-center items-center">
+      <p>{cryptoData[0].current_price}</p>
+    </main>
+  );
 }
